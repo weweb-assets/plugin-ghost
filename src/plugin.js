c70c5b05fe1595454955dcedc07d28113ee79d4b
Damien DURO <damien@weweb.io>
2021-02-23 19:41:58 +0100

    fix isFirstTime

--- src/plugin.js
+++ src/plugin.js
@@ -24,9 +24,12 @@ export default {
         /* wwEditor:start */
         const plugin = wwLib.wwPlugins.pluginGhost;
         if (plugin.id) plugin.settings = (await wwLib.wwPlugin.getSettings(plugin.id)) || this.settings;
+        if (!plugin.settings.privateData.contentApiKey) plugin.settings.privateData.contentApiKey = '';
+        if (!plugin.settings.privateData.adminApiKey) plugin.settings.privateData.adminApiKey = '';
+        if (!plugin.settings.privateData.endpoints) plugin.settings.privateData.url = '';
         if (!plugin.settings.privateData.endpoints) plugin.settings.privateData.endpoints = [];
-        if (!plugin.settings.privateData.url) {
-            plugin.settings.privateData.url = '';
+        const { contentApiKey, adminApiKey, url } = plugin.settings.privateData;
+        if (!contentApiKey.length || !adminApiKey.length || !url.length) {
             this.sidebarButton();
         }
         /* wwEditor:end */
@@ -38,10 +41,12 @@ export default {
     async sidebarButton() {
         try {
             const { id, settings } = wwLib.wwPlugins.pluginGhost;
+            const { contentApiKey, adminApiKey, url } = settings.privateData;
+            const isFirstTime = !contentApiKey.length || !adminApiKey.length || !url.length;
             await wwLib.wwPopups.open({
                 firstPage: settings.privateData.url ? 'GHOST_POPUP' : 'GHOST_CONFIGURATION_POPUP',
                 data: {
-                    isFirstTime: !settings.privateData.url,
+                    isFirstTime,
                     pluginId: id,
                     settings,
                 },
